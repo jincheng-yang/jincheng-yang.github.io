2f6340bbca1305080b08a1fea3005c72651bf39e
Teddy Young <teddy_young@qq.com>
2017-10-21 08:52:11 -0500

    test latex

---
+++ plugin/drag-and-drop.js
@@ -0,0 +1,11 @@
+function allowDrop(ev) {
+	ev.preventDefault();
+}
+
+function drag(ev) {
+	ev.dataTransfer.setData("Text",ev.target.id);
+}
+
+function drop(ev) {
+	window.location.href="/assets/files/Jincheng Yang.pdf";
+}
