5be66cf0c0d9488222ac51c52a5d0a4eb1c5ee12
Jincheng Yang <623885838@qq.com>
2019-02-23 16:19:20 -0600

    Update blog

---
+++ _site/users/jcyang/plugin/2d-linear-dynamical-system.js
@@ -0,0 +1,113 @@
+var canvas2 = document.getElementById("mycanvas2");
+var ctx2 = canvas2.getContext("2d");
+
+var window_width = 8;
+var window_height = 8;
+var window_left = -window_width / 2;
+var window_right = window_width / 2;
+var window_top = window_height / 2;
+var window_bottom = -window_height / 2;
+
+var scaling_width = canvas2.width / window_width;
+var scaling_height = canvas2.height / window_height;
+var stroke_width = 1.00 / scaling_width;
+
+ctx2.translate(-window_left * scaling_width, window_top * scaling_height);
+ctx2.scale(scaling_width, scaling_height);
+ctx2.lineWidth = stroke_width;
+
+var DT = 0.002;
+var agents = [];
+let AGENT_COUNT = 500;
+
+class Agent {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.dx = 0;
+    this.dy = 0;
+  }
+  
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+    this.show();
+    this.calculateSpeed();
+    if (Math.random() < 0.01 | this.x < window_left | this.x > window_right | this.y < window_bottom | this.y > window_top) {
+    	this.x = window_left + Math.random() * window_width;
+        this.y = window_bottom + Math.random() * window_height;
+    }
+  }
+  
+  calculateSpeed() {
+    this.dx = (a11 * this.x + a12 * this.y) * DT;
+    this.dy = (a21 * this.x + a22 * this.y) * DT;
+  }
+  
+  show() {
+    ctx2.beginPath();
+    ctx2.arc(this.x, -this.y, stroke_width, 0, 2 * Math.PI);
+    ctx2.stroke();
+  }
+}
+
+for (var i = 0; i < AGENT_COUNT; i++) {
+  agents.push(new Agent((Math.random() - 0.5) * window_width, (Math.random() - 0.5) * window_height));
+}
+
+updateArgument();
+window.requestAnimationFrame(draw);
+
+function draw() {
+  clear();
+  drawGrid();
+    
+  for (i = 0; i < AGENT_COUNT; i++) {
+    agents[i].update();
+  }
+
+  window.requestAnimationFrame(draw);
+}
+
+function clear() {
+  ctx2.fillStyle = 'rgba(250, 250, 250, 0.02)';
+  ctx2.fillRect(window_left, -window_top, window_width, window_height);
+}
+
+function clearAll() {
+  ctx2.fillStyle = 'white';
+  ctx2.fillRect(window_left, -window_top, window_width, window_height);
+}
+
+function updateArgument() {
+	a11 = document.getElementById("a11").value;
+	a12 = document.getElementById("a12").value;
+	a21 = document.getElementById("a21").value;
+  a22 = document.getElementById("a22").value;
+  det = a11 * a22 - a12 * a21;
+  tr = a11 * 1 + a22 * 1;
+  document.getElementById("det_tr").innerHTML = "determinent = " + det + ", trace = " + tr + ".";
+  document.getElementById("cir").setAttribute("cx", tr / 4 * 50);
+  document.getElementById("cir").setAttribute("cy", -det / 16 * 50);
+  clearAll();
+}
+
+function drawGrid() {
+  ctx2.strokeStyle = 'rgba(128, 128, 128, 0.3)';
+  for (i = Math.floor(window_left); i <= Math.ceil(window_right); i++) {
+    line(i, -window_top, i, -window_bottom);
+  }
+  for (j = Math.floor(window_bottom); j <= Math.ceil(window_top); j++) {
+    line(window_left, -j, window_right, -j);
+  }
+  ctx2.strokeStyle = 'rgba(0, 0, 32, 0.8)';
+  line(0, -window_top, 0, -window_bottom);
+  line(window_left, 0, window_right, 0);
+}
+
+function line(x1, y1, x2, y2) {
+  ctx2.beginPath();
+  ctx2.moveTo(x1, y1);
+  ctx2.lineTo(x2, y2);
+  ctx2.stroke();
+}
---
+++ users/jcyang/teach/2019/m427j/index.md
@@ -0,0 +1,12 @@
+---
+layout: general
+title: M427J Differential Equations with Linear Algebra
+outer-class: Teaching Assistant
+---
+
+Syllabus: Check on canvas.
+
+- Regular Class: TTh 3:00-4:30 pm @ ECJ 1.202
+- Discussion Session: MW 1:00-2:00 pm @ ETC 2.108
+- Dr. Mills' Office Hour: TTh 9:00-10:30 am, 2:00 pm-3:00 pm @ RLM 12.144
+- My Office Hour: MWF 10:00-11:00 am @ RLM 11.130
